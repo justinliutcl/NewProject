bc97bb9b67feee3906715e1b0e4caff93c808ec1
Your Name <you@example.com>
2017-08-04 21:38:03 +0800

    fix debug Signed-off-by: Your Name <you@example.com>

--- app/src/main/java/util/EidHttpUtil.java
+++ app/src/main/java/util/EidHttpUtil.java
@@ -2,22 +2,13 @@ package util;
 
 import android.util.Base64;
 
-import com.alipay.security.mobile.module.commonutils.crypto.Hex;
-import com.google.zxing.common.StringUtils;
-
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -25,8 +16,8 @@ import java.util.UUID;
  */
 
 public class EidHttpUtil {
-    public static JSONObject getConfig(){
-        JSONObject sig_json=new JSONObject();
+    public static JSONObject getConfig() {
+        JSONObject sig_json = new JSONObject();
         try {
             sig_json.put("version", "1.0.0");
             sig_json.put("return_url", "");
@@ -37,41 +28,41 @@ public class EidHttpUtil {
             String encrypt_factor = uuid.substring(uuid.length() - 16);
             String uuid2 = UUID.randomUUID().toString().replace("-", "");
             String sign_factor = uuid2.substring(uuid2.length() - 16);
-            Map factor=new HashMap();
+            Map factor = new HashMap();
             factor.put("encrypt_factor", encrypt_factor);
             factor.put("sign_factor", sign_factor);
-            sig_json.put("security_factor",new JSONObject(factor));
+            sig_json.put("security_factor", new JSONObject(factor));
             sig_json.put("encrypt_type", "1");
             sig_json.put("sign_type", "1");
             sig_json.put("security_type", "10");
             sig_json.put("attach", "");
             sig_json.put("extension", new JSONObject()); // eid扩展信息
             //biz_type
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
         return sig_json;
     }
 
-    public static JSONObject getConfig2(String encrypt_factor,String sign_factor,String biz_sequence,String time){
-        JSONObject sig_json=new JSONObject();
+    public static JSONObject getConfig2(String encrypt_factor, String sign_factor, String biz_sequence, String time) {
+        JSONObject sig_json = new JSONObject();
         try {
             sig_json.put("version", "1.0.0");
             sig_json.put("return_url", "");
             sig_json.put("appId", "02JR1610081555371526");
             sig_json.put("biz_time", time);
             sig_json.put("biz_sequence_id", biz_sequence);
-            Map factor=new HashMap();
+            Map factor = new HashMap();
             factor.put("encrypt_factor", encrypt_factor);
             factor.put("sign_factor", sign_factor);
-            sig_json.put("security_factor",new JSONObject(factor));
+            sig_json.put("security_factor", new JSONObject(factor));
             sig_json.put("encrypt_type", "1");
             sig_json.put("sign_type", "1");
             sig_json.put("security_type", "10");
             sig_json.put("attach", "");
             sig_json.put("extension", new JSONObject()); // eid扩展信息
             //biz_type
-        }catch (Exception e){
+        } catch (Exception e) {
 
         }
         return sig_json;
@@ -79,19 +70,19 @@ public class EidHttpUtil {
 
     private static SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
 
-    public static String getBizTime(){
+    public static String getBizTime() {
         Date d = new Date();
         return format.format(d);
     }
 
-    public static String getRandom16Number(){
+    public static String getRandom16Number() {
         byte[] b16 = new byte[16];
         Random r = new Random();
         r.nextBytes(b16);
         return Base64.encodeToString(b16, Base64.NO_WRAP);
     }
 
-    public static String createUUID(){
+    public static String createUUID() {
         return UUID.randomUUID().toString().replace("-", "");
     }
 
